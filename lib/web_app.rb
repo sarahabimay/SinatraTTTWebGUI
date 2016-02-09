2ae7557ef0a0e5216d452db6efd00c10fecb14b8
sarahabimay <sarahabimay@gmail.com>
2016-02-09 18:16:10 +0000

    Include Dimension and Human Vs Human game type from tictactoe gem

--- lib/web_app.rb
+++ lib/web_app.rb
@@ -1,10 +1,15 @@
 require "sinatra/base"
 require "tilt/erb"
+require "tictactoe/board_options"
+require "tictactoe/game_type_options"
 
 class WebApp < Sinatra::Base
   set :views, File.dirname(__FILE__) + '/../views'
-  
+  enable :sessions
+
   get '/' do
-    erb :index
+    @dimensions = TicTacToe::BoardOptions::DIMENSIONS
+    @game_types = TicTacToe::GameTypeOptions::GAME_OPTIONS
+    erb :game_options
   end
 end
